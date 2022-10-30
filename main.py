e4b8383697f1a5f461a1d1b7c4362bbc164e4d15
MNVolkov <mr-volkov@yandex.ru>
2022-10-30 21:18:07 +0600

    Убрал лишний маршрут

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from sklearn.linear_model import LinearRegression
 app = flask.Flask(__name__, template_folder = 'templates')
 
 @app.route('/', methods=['POST', 'GET'])
-@app.route('/index', methods=['POST', 'GET'])
 def main():
     if flask.request.method == 'GET':
         return render_template('main.html')
