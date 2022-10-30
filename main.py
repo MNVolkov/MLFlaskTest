c7a409300bdb362c1780d8796286c3d39f77fd4f
MNVolkov <mr-volkov@yandex.ru>
2022-10-30 21:49:38 +0600

    Test

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 import flask
 from flask import render_template
 import pickle
@@ -23,3 +24,13 @@ def main():
 
 if __name__ == '__main__':
     app.run()
+
+"""
+
+from flask import Flask, Response
+app = Flask(__name__)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def catch_all(path):
+    return Response("<h1>Flask</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
